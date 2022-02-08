0508eabd06aa55cdaee61e7c801bf5ca7dfa6020
Mokronos <sebastian@hirts.de>
2022-02-08 18:18:33 +0100

    spellfiles and added comment to markdown about spellfile cleaning

--- nvim/.config/nvim/ftplugin/markdown.lua
+++ nvim/.config/nvim/ftplugin/markdown.lua
@@ -3,5 +3,7 @@ local opt = vim.opt_local
 opt.spell = true
 
 
-local opts = { noremap=true, silent=true}
+local opts = {noremap=true, silent=true}
 vim.api.nvim_set_keymap('i', '<C-s>', '<C-g>u<Esc>[s1z=`]a<C-g>u', opts)
+
+-- spellfile probably should be cleaned at some point, but spellclean.vim is not in neovim. could do it myself (regex)
