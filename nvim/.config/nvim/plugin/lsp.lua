19d28928a8d1dc39691a05dddfc53c4762590618
Mokronos <sebastian@hirts.de>
2022-02-11 04:12:17 +0100

    made keybinds only work on attach

--- nvim/.config/nvim/plugin/lsp.lua
+++ nvim/.config/nvim/plugin/lsp.lua
@@ -1,14 +1,24 @@
 -- autocomplete --> lsp connection
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 
+-- set up keybinds, only use them on attach to server
+local opts = { noremap=true, silent=true}
+local on_attach = function(client, bufnr)
+    vim.api.nvim_set_keymap('n', '<Leader>K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
+    vim.api.nvim_set_keymap('n', '<Leader>R', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
+    vim.api.nvim_set_keymap('n', '<Leader>vs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
+    vim.api.nvim_set_keymap('n', '<Leader>vi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
+    vim.api.nvim_set_keymap('n', '<Leader>vc', '<cmd>lua vim.diagnostic.open_float()<cr>', opts)
+end
+
 -- lsp
 local lsp_installer = require("nvim-lsp-installer")
 
 -- does not make sence yet, capabilities work with python even if only given to sumneko_lua
 lsp_installer.on_server_ready(function(server)
-    local opts = {}
+    local server_opts = {}
     if server.name == "sumneko_lua" then
-        opts = {
+        server_opts = {
             settings = {
                 Lua ={
                     diagnostics = {
@@ -16,34 +26,25 @@ lsp_installer.on_server_ready(function(server)
                     },
                 }
             },
-            capabilities = capabilities
+            capabilities = capabilities,
+            on_attach = on_attach,
         }
     end
     if server.name == "pyright" then
-        opts = {
+        server_opts = {
             settings = {
             },
-            capabilities = capabilities
+            capabilities = capabilities,
+            on_attach = on_attach,
         }
     end
-    server:setup(opts)
+    server:setup(server_opts)
 end)
 
+-- Hover doc popup
 local lsp = vim.lsp
 local handlers = lsp.handlers
 
--- Hover doc popup
 local pop_opts = { border = "rounded", max_width = 80 }
 handlers["textDocument/hover"] = lsp.with(handlers.hover, pop_opts)
 handlers["textDocument/signatureHelp"] = lsp.with(handlers.signature_help, pop_opts)
-
-
-local opts = { noremap=true, silent=true}
-
-
-vim.api.nvim_set_keymap('n', '<Leader>d', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
-vim.api.nvim_set_keymap('n', '<Leader>K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
-vim.api.nvim_set_keymap('n', '<Leader>R', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
-vim.api.nvim_set_keymap('n', '<Leader>vs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
-vim.api.nvim_set_keymap('n', '<Leader>vi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
-vim.api.nvim_set_keymap('n', '<Leader>vc', '<cmd>lua vim.diagnostic.open_float()<cr>', opts)
