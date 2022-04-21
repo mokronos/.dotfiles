3b4b2e98306920c94c01ca7fd2b8d7f6a21ca5f5
Mokronos <sebastian@hirts.de>
2022-04-21 11:30:28 +0200

    fixed completion and switched to luasnip

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -38,10 +38,8 @@ Plug 'hrsh7th/cmp-cmdline'
 Plug 'hrsh7th/nvim-cmp'
 
 -- snippetengine(kinda required for nvim-cmp)
-Plug 'SirVer/ultisnips'
-Plug 'quangnguyen30192/cmp-nvim-ultisnips'
--- snipets
-Plug 'honza/vim-snippets'
+Plug 'L3MON4D3/LuaSnip'
+Plug 'saadparwaiz1/cmp_luasnip'
 
 -- Plug 'ambv/black'
 
