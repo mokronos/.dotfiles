5426834af2a0d34b407e9e514e1cda21ad51cc43
Mokronos <sebastian@hirts.de>
2022-04-27 15:41:32 +0200

    added spellfiles and fixed spell (just used vimscript)

--- nvim/.config/nvim/ftplugin/markdown.lua
+++ nvim/.config/nvim/ftplugin/markdown.lua
@@ -1,10 +1,15 @@
 -- make options local
-local opt = vim.opt_local
-opt.spell = true
-opt.spelllang = en_us,de_de
+-- local opt = vim.opt_local
+-- opt.spell = true
+-- opt.spelllang = en_us,de_de
+
+-- convert to lua (above stuff is not working for some reason)
+vim.api.nvim_command('setlocal spell spelllang=en_us,de_de')
 
 
 local opts = {noremap=true, silent=true}
 vim.api.nvim_set_keymap('i', '<C-s>', '<C-g>u<Esc>[s1z=`]a<C-g>u', opts)
+
+print("markdown file loaded")
 
 -- spellfile probably should be cleaned at some point, but spellclean.vim is not in neovim. could do it myself (regex)
