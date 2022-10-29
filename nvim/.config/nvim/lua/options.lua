5986fd893d6be0073d04a21fe7a8ff43a7904240
mokronos <sebastian@hirts.de>
2022-10-29 04:05:44 +0200

    fixed indent for comments

--- nvim/.config/nvim/lua/options.lua
+++ nvim/.config/nvim/lua/options.lua
@@ -15,7 +15,7 @@ opt.tabstop=4
 opt.softtabstop=4
 opt.shiftwidth=4
 opt.expandtab = true
-opt.smartindent = true
+-- opt.smartindent = true
 
 opt.termguicolors = true
 opt.wildmode='longest,list,full'
