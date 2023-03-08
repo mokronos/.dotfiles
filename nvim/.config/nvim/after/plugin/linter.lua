8536ff50966fdb400912339e948593eb4fd3d750
Sebastian Hirt <sebastian@hirts.de>
2023-03-08 02:44:07 +0100

    setup linter

---
+++ nvim/.config/nvim/after/plugin/linter.lua
@@ -0,0 +1,14 @@
+require("mason-null-ls").setup({
+    ensure_installed = {
+        -- Opt to list sources here, when available in mason.
+    },
+    automatic_installation = false,
+    automatic_setup = true, -- Recommended, but optional
+})
+require("null-ls").setup({
+    sources = {
+        -- Anything not supported by mason.
+    }
+})
+
+require 'mason-null-ls'.setup_handlers() -- If `automatic_setup` is true.
