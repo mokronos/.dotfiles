42306b9bb8e0b77ed600d296827bc7febf421735
Sebastian Hirt <sebastian@hirts.de>
2023-04-03 17:25:30 +0200

    feat: Add text file completion

---
+++ nvim/.config/nvim/ftplugin/text.lua
@@ -0,0 +1,15 @@
+local opt = vim.opt_local
+
+-- convert to lua (above stuff is not working for some reason)
+vim.api.nvim_command('setlocal spell spelllang=en_us,de_de')
+
+-- quick fix last wrong word in insert mode
+vim.keymap.set('i', '<C-s>', '<C-g>u<Esc>[s1z=`]a<C-g>u', {desc = 'fix last spelling error'})
+
+-- open dictionary autocomplete
+vim.keymap.set('i', '<C-c>', '<C-x><C-k>', {desc = 'dictionary autocomplete'})
+
+-- spellfile probably should be cleaned at some point, but spellclean.vim is not in neovim. could do it myself (regex)
+
+opt.wrap = true
+opt.linebreak = true
