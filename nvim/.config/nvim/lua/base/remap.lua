b8537f92c0bf514c756764fda66da2cf3c06630f
Sebastian Hirt <sebastian@hirts.de>
2023-04-05 03:57:24 +0200

    Feat: Add remap of ctrl-^ because its difficult to reach

--- nvim/.config/nvim/lua/base/remap.lua
+++ nvim/.config/nvim/lua/base/remap.lua
@@ -20,7 +20,10 @@ vim.keymap.set("n", "<leader>Y", [["+Y]], {desc = '[Y]ank from cursor to end of 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", {desc = 'Move selected lines down'})
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", {desc = 'Move selected lines up'})
 
+vim.keymap.set("n", "<C-j>", "<C-^>", {desc = 'Jump to alternate file'})
+
 -- no clue why this doesn't work
 -- vim.keymap.set("n", "<leader>;", function()
 --     vim.cmd("messages")
 -- end, {desc = 'Show messages'})
+
