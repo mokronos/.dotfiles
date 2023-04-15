502b60b06c27218229256d08fbfde4f6afe88440
Sebastian Hirt <sebastian@hirts.de>
2023-04-15 06:51:13 +0200

    Feat: Add markdown previewer, settings not all lua ready

--- nvim/.config/nvim/lua/base/plugins.lua
+++ nvim/.config/nvim/lua/base/plugins.lua
@@ -73,5 +73,11 @@ return require('packer').startup(function(use)
     -- debugging
     use 'mfussenegger/nvim-dap'
 
+    -- markdown
+    use({ "iamcco/markdown-preview.nvim",
+    run = "cd app && npm install",
+    setup = function() vim.g.mkdp_filetypes = { "markdown" } end,
+    ft = { "markdown" }, })
+
 
 end)
