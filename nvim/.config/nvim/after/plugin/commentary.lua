92412ab8ff56738f93ebabcd2684f1b513bef6f4
mokronos <sebastian@hirts.de>
2023-11-06 17:13:37 +0100

    Add special comment for jinja

--- nvim/.config/nvim/after/plugin/commentary.lua
+++ nvim/.config/nvim/after/plugin/commentary.lua
@@ -1,2 +1,7 @@
 vim.keymap.set('', '<Leader>c', '<Plug>Commentary', {desc = '[C]ommentary operator'})
 vim.keymap.set('n', '<Leader>cc', '<Plug>CommentaryLine', {desc = '[C]omment line toggle'})
+
+-- works for now, but need better solution + some html not recognized as jinja template
+vim.cmd([[
+autocmd FileType htmldjango setlocal commentstring={#%s#}
+]])
