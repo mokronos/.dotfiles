9db8dbe4e32857fb78649e6f2a2df33b7af16932
Sebastian Hirt <sebastian@hirts.de>
2024-08-28 14:04:37 +0200

    Add correct comment strings

--- nvim/.config/nvim/after/plugin/commentary.lua
+++ nvim/.config/nvim/after/plugin/commentary.lua
@@ -1,7 +1,24 @@
 vim.keymap.set('', '<Leader>c', '<Plug>Commentary', {desc = '[C]ommentary operator'})
 vim.keymap.set('n', '<Leader>cc', '<Plug>CommentaryLine', {desc = '[C]omment line toggle'})
 
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "cuda",
+    callback = function()
+        vim.bo.commentstring = "// %s"
+    end
+})
+
 -- works for now, but need better solution + some html not recognized as jinja template
-vim.cmd([[
-autocmd FileType htmldjango setlocal commentstring={#%s#}
-]])
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "htmldjango",
+    callback = function()
+        vim.bo.commentstring = "{# %s #}"
+    end
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "cpp",
+    callback = function()
+        vim.bo.commentstring = "// %s"
+    end
+})
