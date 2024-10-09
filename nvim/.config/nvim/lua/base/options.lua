36de9659ae599c21604ba392f8949ceb39793c40
Sebastian Hirt <sebastian@hirts.de>
2024-10-09 19:25:32 +0200

    Add system python as python provider

--- nvim/.config/nvim/lua/base/options.lua
+++ nvim/.config/nvim/lua/base/options.lua
@@ -53,4 +53,4 @@ vim.g.tex_flavor='latex'
 vim.opt.formatoptions:remove 'o'
 
 -- make system python the default, otherwise it uses the one from the venv
--- vim.g.python3_host_prog='/usr/bin/python3'
+vim.g.python3_host_prog='/usr/bin/python3'
