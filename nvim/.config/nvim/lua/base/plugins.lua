93fb4f6fb1c279b6d9c090a29d7e407ccc68fc95
Sebastian Hirt <sebastian@hirts.de>
2024-11-04 12:38:39 +0100

    Switch to codeium from copilot

--- nvim/.config/nvim/lua/base/plugins.lua
+++ nvim/.config/nvim/lua/base/plugins.lua
@@ -144,8 +144,13 @@ require('lazy').setup({
     -- Better undo
     'mbbill/undotree',
 
-    -- Copilot
-    'zbirenbaum/copilot.lua',
+    -- LLM completion
+    {'zbirenbaum/copilot.lua', enabled = false},
+    { "supermaven-inc/supermaven-nvim", enabled = false },
+    {
+        "Exafunction/codeium.vim",
+        event = 'BufEnter'
+    },
 
     -- markdown
     {
@@ -185,5 +190,54 @@ require('lazy').setup({
         'nosduco/remote-sshfs.nvim',
         dependencies = { "nvim-telescope/telescope.nvim" },
         opts = {},
+    },
+
+    -- ai, getting tiktoken_core error, not sure if it doens't work because of that
+    {
+        "yetone/avante.nvim",
+        event = "VeryLazy",
+        lazy = false,
+        enabled = false,
+        version = false, -- set this if you want to always pull the latest change
+        opts = {
+            provider = "copilot",
+        },
+        -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+        build = "make",
+        -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+        dependencies = {
+            "nvim-treesitter/nvim-treesitter",
+            "stevearc/dressing.nvim",
+            "nvim-lua/plenary.nvim",
+            "MunifTanjim/nui.nvim",
+            --- The below dependencies are optional,
+            "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+            "zbirenbaum/copilot.lua", -- for providers='copilot'
+            {
+                -- support for image pasting
+                "HakonHarnes/img-clip.nvim",
+                event = "VeryLazy",
+                opts = {
+                    -- recommended settings
+                    default = {
+                        embed_image_as_base64 = false,
+                        prompt_for_file_name = false,
+                        drag_and_drop = {
+                            insert_mode = true,
+                        },
+                        -- required for Windows users
+                        use_absolute_path = true,
+                    },
+                },
+            },
+            {
+                -- Make sure to set this up properly if you have lazy=true
+                'MeanderingProgrammer/render-markdown.nvim',
+                opts = {
+                    file_types = { "markdown", "Avante" },
+                },
+                ft = { "markdown", "Avante" },
+            },
+        },
     }
 }, {})
