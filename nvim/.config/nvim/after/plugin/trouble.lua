59fca82cd3ec938cc14723333f2907d322a47d52
Sebastian Hirt <sebastian@hirts.de>
2024-12-10 12:08:08 +0100

    Fix trouble

--- nvim/.config/nvim/after/plugin/trouble.lua
+++ nvim/.config/nvim/after/plugin/trouble.lua
@@ -5,7 +5,7 @@ trouble.setup {
     -- refer to the configuration section below
 }
 
-vim.keymap.set("n", "<Leader>tt", function() trouble.toggle() end , {desc = "[T]rouble [T]oggle"})
+vim.keymap.set("n", "<Leader>tt", function() trouble.toggle('diagnostics') end , {desc = "[T]rouble [T]oggle"})
 vim.keymap.set("n", "<Leader>tw", function() trouble.toggle("workspace_diagnostics") end , {desc = "[T]rouble [W]orkspace diagnostics"})
 vim.keymap.set("n", "<Leader>tq", function() trouble.toggle("quickfix") end , {desc = "[T]rouble [Q]uickfix"})
 vim.keymap.set("n", "<Leader>td", function() trouble.toggle("document_diagnostics") end , {desc = "[T]rouble [D]ocument diagnostics"})
