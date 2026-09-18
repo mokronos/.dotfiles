54ea1c34309501c356027281d179503ca0ac75a7
Sebastian Hirt <sebastian@hirts.de>
2026-09-18 18:03:32 +0200

    hypr: swap caps and escape

--- hypr/.config/hypr/input.lua
+++ hypr/.config/hypr/input.lua
@@ -1,6 +1,7 @@
 hl.config({
   input = {
     kb_layout = "us",
+    kb_options = "caps:swapescape,shift:both_capslock_cancel",
     repeat_rate = 40,
     repeat_delay = 600,
     numlock_by_default = true,
